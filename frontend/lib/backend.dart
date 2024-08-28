41f0eca8d388514ecaa158e97add5b40b2693b10
Seung Cha <seunghwancha5857@gmail.com>
2024-08-28 18:36:10 +1000

    comply with naming conventions

--- frontend/lib/backend.dart
+++ frontend/lib/backend.dart
@@ -5,14 +5,14 @@ import 'dart:convert';
 // Helper for calling backend functions
 
 class Backend {
-  static final String _url = '127.0.0.1:5000';
-  static Future<KindlePage> GetPage(String id, int i) async {
+  static const String _url = '127.0.0.1:5000';
+  static Future<KindlePage> getPage(String id, int i) async {
     final res =
         await http.get(Uri.http(_url, '/get', {'id': id, 'no': i.toString()}));
     return KindlePage.fromJson(json.decode(res.body) as Map<String, dynamic>);
   }
 
-  static Future<BookMetadata> OpenFile(String path) async {
+  static Future<BookMetadata> openFile(String path) async {
     final res = await http.post(Uri.http(_url, 'open', {'path': path}));
     return BookMetadata.fromJson(json.decode(res.body) as Map<String, dynamic>);
   }
